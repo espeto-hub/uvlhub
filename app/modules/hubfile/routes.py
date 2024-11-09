92795c39894dc7ce6fc3a295fc6e99f73616d881
Francisco Perez Lazaro <fraperper5@alum.us.es>
2024-11-09 10:38:54 +0100

    feat(WI): added Download all datasets

--- app/modules/hubfile/routes.py
+++ app/modules/hubfile/routes.py
@@ -4,7 +4,49 @@ import uuid
 from flask import current_app, jsonify, make_response, request, send_from_directory
 from flask_login import current_user
 from app.modules.hubfile import hubfile_bp
+from app.modules.hubfile.models import HubfileDownloadRecord, HubfileViewRecord
+from app.modules.hubfile.services import HubfileDownloadRecordService, HubfileService
 from app import db
+
+
+@hubfile_bp.route("/file/download/<int:file_id>", methods=["GET"])
+def download_file(file_id):
+    file = HubfileService().get_or_404(file_id)
+    filename = file.name
+
+    directory_path = f"uploads/user_{file.feature_model.data_set.user_id}/dataset_{file.feature_model.data_set_id}/"
+    parent_directory_path = os.path.dirname(current_app.root_path)
+    file_path = os.path.join(parent_directory_path, directory_path)
+
+    # Get the cookie from the request or generate a new one if it does not exist
+    user_cookie = request.cookies.get("file_download_cookie")
+    if not user_cookie:
+        user_cookie = str(uuid.uuid4())
+
+    # Check if the download record already exists for this cookie
+    existing_record = HubfileDownloadRecord.query.filter_by(
+        user_id=current_user.id if current_user.is_authenticated else None,
+        file_id=file_id,
+        download_cookie=user_cookie
+    ).first()
+
+    if not existing_record:
+        # Record the download in your database
+        HubfileDownloadRecordService().create(
+            user_id=current_user.id if current_user.is_authenticated else None,
+            file_id=file_id,
+            download_date=datetime.now(timezone.utc),
+            download_cookie=user_cookie,
+        )
+
+    # Save the cookie to the user's browser
+    resp = make_response(
+        send_from_directory(directory=file_path, path=filename, as_attachment=True)
+    )
+    resp.set_cookie("file_download_cookie", user_cookie)
+
+    return resp
+
 
 @hubfile_bp.route('/file/view/<int:file_id>', methods=['GET'])
 def view_file(file_id):
