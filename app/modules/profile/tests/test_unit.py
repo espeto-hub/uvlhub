deeaef633e696e72966b61e4e0970a3a99bea03f
gonzalodom11 <jgdominguezmoreno@gmail.com>
2024-11-27 10:22:26 +0100

    fix: delete some profile unit tests

--- app/modules/profile/tests/test_unit.py
+++ app/modules/profile/tests/test_unit.py
@@ -1,5 +1,3 @@
-from flask import url_for
-from flask_login import login_user
 import pytest
 
 from app import db
@@ -38,17 +36,3 @@ def test_edit_profile_page_get(test_client):
     assert b"Edit profile" in response.data, "The expected content is not present on the page"
 
     logout(test_client)
-
-
-def test_user_profile_not_found(client):
-    """Test para verificar que se devuelve un 404 si el usuario no existe"""
-    response = client.get(url_for('profile.user_profile', user_id=9999))  # Suponiendo que el user_id 9999 no existe
-    assert response.status_code == 404
-
-
-def test_user_profile_found(user, client):
-    """Test para verificar que el perfil de un usuario válido se carga correctamente"""
-    login_user(user, client)
-    response = client.get(url_for('profile.user_profile', user_id=user.id))
-    assert response.status_code == 200
-    assert b'User Profile' in response.data  # Verificar que el perfil del usuario está presente
