6b28f019d267b8cf7bd26da69cac5cc2711be40d
kiinami <kinami@posteo.net>
2024-11-27 10:06:47 +0100

    fix: duplicated test

--- app/modules/hubfile/tests/test_unit.py
+++ app/modules/hubfile/tests/test_unit.py
@@ -1,6 +1,8 @@
 import os
 from unittest.mock import patch
 
+import pytest
+
 from app.modules.featuremodel.repositories import FeatureModelRepository
 from app.modules.hubfile.repositories import HubfileRepository
 from app.modules.dataset.repositories import DataSetRepository
@@ -10,22 +12,7 @@ from app.modules.auth.repositories import UserRepository
 from dotenv import load_dotenv
 
 
-def test_create_hubfile_calls_enqueue_task(test_client):
-    with patch("core.managers.task_queue_manager.TaskQueueManager.enqueue_task") as mock_enqueue_task:
-        user = UserRepository().create(password="foo")
-        dsmetadata = DSMetaDataRepository().create(
-            title="test",
-            description="test",
-            publication_type=PublicationType.BOOK,
-        )
-        dataset = DataSetRepository().create(user_id=user.id, ds_meta_data_id=dsmetadata.id)
-        fm = FeatureModelRepository().create(data_set_id=dataset.id)
-        HubfileRepository().create(
-            name="test.uvl",
-            checksum="1234",
-            size=1234,
-            feature_model_id=fm.id,
-        )
+@pytest.fixture(scope="module")
 def test_create_hubfile_calls_enqueue_task(test_client):
     with patch("core.managers.task_queue_manager.TaskQueueManager.enqueue_task") as mock_enqueue_task:
         user = UserRepository().create(password="foo")
@@ -58,6 +45,6 @@ def test_create_hubfile_calls_enqueue_task(test_client):
         # Verificar que enqueue_task fue llamado correctamente
         mock_enqueue_task.assert_called_once_with(
             "app.modules.hubfile.tasks.transform_uvl",  # Nombre de la tarea
-            path=path  # Parámetro que recibe la tarea
+            path=path,  # Parámetro que recibe la tarea
+            timeout=300
         )
-        
