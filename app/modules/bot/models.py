0150ba57c6f2ed1c40e727590657ae2dc58bfdb2
kiinami <kinami@posteo.net>
2024-12-05 12:14:05 +0100

    feat: added service name field to model

--- app/modules/bot/models.py
+++ app/modules/bot/models.py
@@ -4,6 +4,7 @@ from app import db
 class Bot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
+    service_name = db.Column(db.String(120), nullable=False)
     args = db.Column(db.JSON, nullable=False)
     enabled = db.Column(db.Boolean, default=True)
     on_download_dataset = db.Column(db.Boolean, default=False)
