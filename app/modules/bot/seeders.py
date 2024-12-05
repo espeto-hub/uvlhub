a42f2ac21c60128cfc1b23d14f4c75e8196d5c37
kiinami <kinami@posteo.net>
2024-12-05 11:50:13 +0100

    feat: added basic bot listing

--- app/modules/bot/seeders.py
+++ app/modules/bot/seeders.py
@@ -1,12 +1,15 @@
+from app.modules.auth.models import User
 from core.seeders.BaseSeeder import BaseSeeder
+from app.modules.bot.models import Bot
 
 
 class BotSeeder(BaseSeeder):
 
     def run(self):
+        user1 = User.query.filter_by(email='user1@example.com').first()
 
         data = [
-            # Create any Model object you want to make seed
+            Bot(name='Bot 1', args={'arg1': 'value1'}, user_id=user1.id),
         ]
 
         self.seed(data)
