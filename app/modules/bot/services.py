64c0df23cf16510cd9ca209e75706cd14e67cb8c
kiinami <kinami@posteo.net>
2024-12-05 11:50:13 +0100

    feat: added basic bot listing

--- app/modules/bot/services.py
+++ app/modules/bot/services.py
@@ -5,3 +5,6 @@ from core.services.BaseService import BaseService
 class BotService(BaseService):
     def __init__(self):
         super().__init__(BotRepository())
+
+    def get_all_by_user(self, user_id):
+        return self.repository.get_all_by_user(user_id)
