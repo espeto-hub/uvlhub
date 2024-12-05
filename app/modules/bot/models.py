a431ba259c0572b5427026af9327b98016b24d9c
kiinami <kinami@posteo.net>
2024-12-05 11:32:59 +0100

    feat: added database model

--- app/modules/bot/models.py
+++ app/modules/bot/models.py
@@ -3,6 +3,13 @@ from app import db
 
 class Bot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    args = db.Column(db.JSON, nullable=False)
+    enabled = db.Column(db.Boolean, default=True)
+    on_download_dataset = db.Column(db.Boolean, default=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref='bots', lazy=True)
 
     def __repr__(self):
-        return f'Bot<{self.id}>'
+        return f'{self.name} bot <{self.id}>'
