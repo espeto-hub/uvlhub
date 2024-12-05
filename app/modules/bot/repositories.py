8130a54c9d48e0bbd2ab907d3be8b5b6cfc51175
kiinami <kinami@posteo.net>
2024-12-05 12:42:12 +0100

    feat: added delete bot functionality

--- app/modules/bot/repositories.py
+++ app/modules/bot/repositories.py
@@ -8,3 +8,9 @@ class BotRepository(BaseRepository):
 
     def get_all_by_user(self, user_id):
         return Bot.query.filter_by(user_id=user_id).all()
+
+    def get_by_id(self, id):
+        return Bot.query.filter_by(id=id).first()
+
+    def delete(self, bot):
+        return bot.delete()
