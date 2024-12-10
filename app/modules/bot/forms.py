be2423835e82aa980e10b253840134fccc7b2579
kiinami <kinami@posteo.net>
2024-12-10 11:28:07 +0100

    feat: added service selection

--- app/modules/bot/forms.py
+++ app/modules/bot/forms.py
@@ -7,7 +7,8 @@ from app import apprise
 
 class CreateBotForm(FlaskForm):
     name = wtforms.StringField('Name', validators=[DataRequired(), Length(min=3, max=50)])
-    service_url = wtforms.URLField('Service URL', validators=[DataRequired()])
+    service_name = wtforms.SelectField('Service', choices=apprise.service_names)
+    service_url = wtforms.URLField('URL', validators=[DataRequired()])
     enabled = wtforms.BooleanField('Enabled', default=True)
     on_download_dataset = wtforms.BooleanField('On download dataset', default=False)
     submit = wtforms.SubmitField('Save')
