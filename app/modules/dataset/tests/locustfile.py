1401f1a6dd771728929d2412dcc339c6f4e10055
manuelcastillejovela <mancasvel@icloud.com>
2024-12-10 19:22:13 +0100

    test: locust test for rate datasets

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,16 +1,36 @@
 from locust import HttpUser, TaskSet, task
 from core.locust.common import get_csrf_token
 from core.environment.host import get_host_for_locust_testing
+from random import randint
 
 
 class DatasetBehavior(TaskSet):
     def on_start(self):
         self.dataset()
 
-    @task
+    @task(5)  
+    def open_dataset(self):
+        dataset_id = randint(1, 100)
+        self.client.get(f"/dataset/view/{dataset_id}")
+        print(f"Opened dataset {dataset_id}")
+
+    @task(3)  
     def dataset(self):
         response = self.client.get("/dataset/upload")
         get_csrf_token(response)
+
+    @task(2)  
+    def dataset_rate(self):
+        for i in range(4):
+            dataset_id = i + 1
+            self.client.post(f"/rate_dataset/{dataset_id}", data={"rate": (i * 2) % 5})
+        print("Rated dataset")
+
+    @task(1)  
+    def search_dataset(self):
+        query = "example"
+        self.client.get(f"/dataset/search?query={query}")
+        print(f"Searched datasets with query: {query}")
 
 
 class DatasetUser(HttpUser):
