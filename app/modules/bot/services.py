5a16a3c6e63bd22dd655402aa8b66def645e9e75
kiinami <kinami@posteo.net>
2024-12-12 12:19:24 +0100

    feat: send test message

--- app/modules/bot/services.py
+++ app/modules/bot/services.py
@@ -17,18 +17,24 @@ class BotService(BaseService):
         return self.repository.delete(bot)
 
     def create_bot(self, form):
-        if form.validate():
-            created_instance = self.create(
-                name=form.name.data,
-                service_name=form.service_name.data,
-                service_url=form.service_url.data,
-                enabled=form.enabled.data,
-                on_download_dataset=form.on_download_dataset.data,
-                user_id=form.user_id.data,
-            )
-            return created_instance, None
-
-        return None, form.errors
+        created_instance = self.create(
+            name=form.name.data,
+            service_name=form.service_name.data,
+            service_url=form.service_url.data,
+            enabled=form.enabled.data,
+            on_download_dataset=form.on_download_dataset.data,
+            user_id=form.user_id.data,
+        )
+        return created_instance, None
 
     def get_by_user_and_name(self, user_id, name):
         return self.repository.get_by_user_and_name(user_id, name)
+
+
+class BotMessagingService(BaseService):
+    def __init__(self):
+        super().__init__(BotRepository())
+
+    @staticmethod
+    def send_test_message(urls: str | list[str]):
+        return apprise.send_test_message(urls)
