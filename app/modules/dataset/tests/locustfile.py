8d0c89d9c673e583a737c1f1a238c1d4f58a2e9f
manuelcastillejovela <mancasvel@icloud.com>
2024-12-12 13:03:18 +0100

    fix: github error

---
+++ app/modules/dataset/tests/locustfile.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, TaskSet, task
+from core.locust.common import get_csrf_token
+from core.environment.host import get_host_for_locust_testing
+class DatasetBehavior(TaskSet):
+    def on_start(self):
+        self.dataset()
+    @task
+    def dataset(self):
+        response = self.client.get("/dataset/upload")
+        get_csrf_token(response)
+
+    # Test de carga para Download all datasets
+    @task
+    def download_all_datasets(self):
+        self.client.get("/dataset/download/all")
+
+
+class DatasetUser(HttpUser):
+    tasks = [DatasetBehavior]
+    min_wait = 5000
+    max_wait = 9000
+    host = get_host_for_locust_testing()
