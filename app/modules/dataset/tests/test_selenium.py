6cd088bf1eb4af0272a3228ff3dd4330c10f95b6
manuelcastillejovela <mancasvel@icloud.com>
2024-12-13 17:22:40 +0100

    fix: delete download dataset test for now

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -26,43 +26,6 @@ def count_datasets(driver, host):
     return amount_datasets
 
 
-def test_download_all_datasets():
-    driver = initialize_driver()
-
-    try:
-        host = get_host_for_selenium_testing()
-
-        # Open the login page
-        driver.get(f"{host}/login")
-        wait_for_page_to_load(driver)
-        # Find the username and password field and enter the values
-        email_field = driver.find_element(By.NAME, "email")
-        password_field = driver.find_element(By.NAME, "password")
-
-        email_field.send_keys("user1@example.com")
-        password_field.send_keys("1234")
-
-        # Send the form
-        password_field.send_keys(Keys.RETURN)
-        wait_for_page_to_load(driver)
-
-        # Open the download all datasets page
-        driver.get(f"{host}/dataset/download/all")
-        wait_for_page_to_load(driver)
-
-        # Verify that the "Download all dataset" link is present in the navigation bar
-        download_link = driver.find_element(By.LINK_TEXT, "Download all dataset")
-        assert download_link.is_displayed(), "Download all dataset link is not displayed in the navigation bar"
-
-        print("Test passed!")
-
-    finally:
-
-        # Close the browser
-        close_driver(driver)
-
-
-test_download_all_datasets()
 
 
 def test_upload_dataset():
